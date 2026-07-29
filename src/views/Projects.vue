96411da0e64c521322c697ca0e31016f8ec5b355
Darren Grants <darrengrants06@gmail.com>
2026-07-29 21:42:35 +0100

    size fix

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,7 +3,7 @@ import ProjectsTabs from '../components/ProjectTabs.vue'
 </script>
 
 <template>
-  <div class="w-[80%] mx-auto p-8 text-white">
+  <div class="max-w-5xl mx-auto px-2 md:px-8 py-8 text-white">
     <h1 class="text-3xl font-bold mb-8">Projects</h1>
     <ProjectsTabs />
   </div>
