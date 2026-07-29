b90fe8a7866f8e5753f5fc4870282fd54907af28
Darren Grants <darrengrants06@gmail.com>
2026-07-29 21:46:51 +0100

    size for desktop fixed

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,7 +3,7 @@ import ProjectsTabs from '../components/ProjectTabs.vue'
 </script>
 
 <template>
-  <div class="max-w-5xl mx-auto px-2 md:px-8 py-8 text-white">
+  <div class="w-full px-2 md:w-[80%] md:px-0 mx-auto py-8 text-white">
     <h1 class="text-3xl font-bold mb-8">Projects</h1>
     <ProjectsTabs />
   </div>
